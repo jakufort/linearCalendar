39e95c0bd733101188b83e2cec228a025a758253
Jakub Fortunka <qba1993@gmail.com>
2018-10-19 23:16:36 +0200

    completely moving to bootstrap

--- elm/src/LinearCalendar.elm
+++ elm/src/LinearCalendar.elm
@@ -11,6 +11,13 @@ import Bootstrap.Grid as Grid
 import Bootstrap.Tab as Tab
 import Bootstrap.CDN as CDN
 import Bootstrap.Utilities.Spacing as Spacing
+import Bootstrap.Form as Form
+import Bootstrap.Form.Input as Input
+import Bootstrap.Form.Select as Select
+import Bootstrap.Button as Button
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
 
 main = Browser.element
                    { init = init
@@ -21,155 +28,135 @@ main = Browser.element
 
 type alias Model = {
     year: String,
-    panel: Panel,
-    tabState: Tab.State
-  }
-
-type alias PDFPanel = {
     cellHeight : String,
-    pageSize : String
+    pageSize : String,
+    generateHtml: Bool,
+    tabState: Tab.State,
+    navbarState: Navbar.State
   }
-
-type alias HTMLPanel = {
-    shouldBeGenerated: Bool
-  }
-
-type Panel = PDF PDFPanel | HTML HTMLPanel
 
 init : () -> (Model, Cmd Msg)
-init _ = ({
-  year = "2018",
-  panel = HTML { shouldBeGenerated = False },
-  tabState = Tab.initialState
-  }, Cmd.none)
+init _ =
+    let
+        (navbarState, navbarCmd) = Navbar.initialState NavbarMsg
+    in
+    ({
+    year = "2018",
+    cellHeight = "10",
+    pageSize = "A4",
+    generateHtml = False,
+    tabState = Tab.initialState,
+    navbarState = navbarState
+    }, navbarCmd)
 
 type Msg = ChangeYear String
   | GenerateHtmlCalendar
   | GeneratePdfCalendar
-  | ChangePanel Panel
   | ChangePageSize String
   | ChangeCellHeight String
   | ChangeTab Tab.State
+  | NavbarMsg Navbar.State
+  | ClearHtmlCalendar
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         ChangeYear newYear -> ({ model | year = newYear }, Cmd.none)
-        GenerateHtmlCalendar -> ({ model | panel = setShouldGenerate (model.panel) }, Cmd.none)
-        GeneratePdfCalendar -> (model, Exporters.PdfExporter.exportCalendar (Exporters.PdfExporter.createModel (PDFExportProperties (getOrDefault (getCellHeight (model.panel)) 10) (getPageSize (model.panel))) (generateCalendar model)))
-        ChangePanel newPanel -> ({ model | panel = newPanel }, Cmd.none)
-        ChangePageSize newPageSize -> ({ model | panel = setPageSize (model.panel) newPageSize }, Cmd.none)
-        ChangeCellHeight newCellHeight -> ({ model | panel = setCellHeight (model.panel) newCellHeight }, Cmd.none)
+        GenerateHtmlCalendar -> ({ model | generateHtml = True }, Cmd.none)
+        GeneratePdfCalendar -> (model, Exporters.PdfExporter.exportCalendar (Exporters.PdfExporter.createModel (PDFExportProperties (getOrDefault (model.cellHeight) 10) (model.pageSize)) (generateCalendar model)))
+        ChangePageSize newPageSize -> ({ model | pageSize = newPageSize }, Cmd.none)
+        ChangeCellHeight newCellHeight -> ({ model | cellHeight = newCellHeight }, Cmd.none)
         ChangeTab state -> ({ model | tabState = state }, Cmd.none)
-
-setShouldGenerate : Panel -> Panel
-setShouldGenerate panel = case panel of
-    PDF _ -> panel
-    HTML values -> HTML ({ values | shouldBeGenerated = True })
-
-setPageSize : Panel -> String -> Panel
-setPageSize panel pageSize = case panel of
-    PDF vals -> PDF ({ vals | pageSize = pageSize })
-    HTML _ -> panel
-
-setCellHeight : Panel -> String -> Panel
-setCellHeight panel cellHeight = case panel of
-    PDF vals -> PDF ({ vals | cellHeight = cellHeight })
-    HTML _ -> panel
-
-getCellHeight : Panel -> String
-getCellHeight panel = case panel of
-    HTML _ -> ""
-    PDF vals -> vals.cellHeight
-
-getPageSize : Panel -> String
-getPageSize panel = case panel of
-    HTML _ -> ""
-    PDF vals -> vals.pageSize
+        NavbarMsg state -> ({ model | navbarState = state }, Cmd.none)
+        ClearHtmlCalendar -> ({ model | generateHtml = False }, Cmd.none)
 
 getOrDefault : String -> Int -> Int
 getOrDefault str def = Maybe.withDefault def (String.toInt(str))
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.batch [
+      Navbar.subscriptions model.navbarState NavbarMsg,
+      Tab.subscriptions model.tabState ChangeTab
+    ]
+
 
 view : Model -> Html Msg
 view model = Grid.container []
   [
-    -- TODO: remove
+  -- TODO: remove
     CDN.stylesheet,
-    Tab.config ChangeTab
-      |> Tab.items [
-          Tab.item {
-            id = "html-tab",
-            link = Tab.link [] [ text "Generate HTML" ],
-            pane = htmlPane model
-          }, Tab.item {
-            id = "pdf-tab",
-            link = Tab.link [] [ text "Generate PDF" ],
-            pane = pdfPane model
-          }
-        ]
-      |> Tab.view model.tabState
+    navbar model,
+    Card.config []
+      |> Card.block []
+          [ Block.titleH4 [] [ text "Calendar Generator" ]
+          , Block.custom <|
+              yearInput
+          , Block.custom <|
+              tabs model
+          ]
+      |> Card.view
   ]
 
---view : Model -> Html Msg
---view model =
---    div []
---    [
---      calendarPanel model
---      , exportTabs model
---    ]
+navbar : Model -> Html Msg
+navbar model = Navbar.config NavbarMsg
+  |> Navbar.withAnimation
+  |> Navbar.attrs [Spacing.mb4]
+  |> Navbar.brand [ href "#"] [ text "Linear Calendar in Elm"]
+  |> Navbar.items
+     [ Navbar.itemLink [href "#"] [ text "Source Code"] ]
+  |> Navbar.view model.navbarState
+
+tabs : Model -> Html Msg
+tabs model = Tab.config ChangeTab
+  |> Tab.pills
+  |> Tab.useHash True
+  |> Tab.items [
+     Tab.item {
+       id = "html-tab",
+       link = Tab.link [] [ text "HTML" ],
+       pane = htmlPane model
+     }, Tab.item {
+       id = "pdf-tab",
+       link = Tab.link [] [ text "PDF" ],
+       pane = pdfPane model
+     }
+   ]
+  |> Tab.view model.tabState
 
 htmlPane : Model -> Tab.Pane Msg
 htmlPane model = Tab.pane
   [ Spacing.mt3 ]
   [
-    h4 [] [ text "HTML PANE" ]
+    Form.form [] [
+      Button.button [ Button.primary, Button.onClick GenerateHtmlCalendar] [ text "Generate" ],
+      Button.button [ Button.secondary, Button.attrs [ Spacing.ml1 ], Button.onClick ClearHtmlCalendar ] [ text "Clear" ],
+      case model.generateHtml of
+          True -> Exporters.HtmlExporter.exportCalendar HTMLExportProperties (generateCalendar model)
+          False -> div [] []
+    ]
   ]
 
 pdfPane : Model -> Tab.Pane Msg
 pdfPane model = Tab.pane
   [ Spacing.mt3 ]
-  [ pdfPanel ]
-
-pdfPanel : Html Msg
-pdfPanel = div [ id "pdf-panel", class "tab-content" ] [
-    div [] [
-        label [ for "page-size-select" ] [ text "Page Size: " ]
-        , select [ id "page-size-select", onInput ChangePageSize ] (List.map (\x -> option [ value x ] [ text x ]) ["A1", "A2", "A3", "A4", "A5"])
-      ]
-    , div [] [
-          label [ for "cell-height-input"] [ text "Cell Height: " ]
-          , input [ id "cell-height-input", onInput ChangeCellHeight ] []
-        ]
-    , button [ onClick GeneratePdfCalendar ] [ text "Generate PDF Calendar" ]
+  [
+    Form.form [] [
+      Form.group [] [
+        Form.label [for "page-size-select"] [ text "Page Size"],
+        Select.select [Select.id "page-size-select", Select.onChange ChangePageSize] (List.map (\x -> Select.item [value x] [text x]) ["A1", "A2", "A3", "A4", "A5"])
+      ],
+      Form.group [] [
+        Input.number [Input.id "cell-height-input", Input.onInput ChangeCellHeight, Input.placeholder "Cell Height"]
+      ],
+      Button.button [Button.primary, Button.onClick GeneratePdfCalendar] [ text "Generate" ]
+    ]
   ]
 
-
---calendarPanel : Model -> Html Msg
---calendarPanel model = div [] [
---    label [ for "calendar-year" ] [ text "Year: "]
---    , input [ value model.year, onInput ChangeYear ] []
---  ]
-
---exportTabs : Model -> Html Msg
---exportTabs model = div [] [
---    button [ onClick (ChangePanel (HTML { shouldBeGenerated = False })) ] [ text "HTML" ]
---    , button [ onClick (ChangePanel (PDF { pageSize = "A4", cellHeight = "10" })) ] [ text "PDF" ]
---    , case model.panel of
---        HTML fields -> htmlPanel model fields.shouldBeGenerated
---        PDF _ -> pdfPanel
---  ]
-
---htmlPanel : Model -> Bool -> Html Msg
---htmlPanel model shouldGenerate = div [ id "html-panel", class "tab-content" ] [
---    button [ onClick GenerateHtmlCalendar ] [ text "Generate HTML Calendar" ]
---    , case shouldGenerate of
---        True -> Exporters.HtmlExporter.exportCalendar HTMLExportProperties (generateCalendar model)
---        False -> div [] []
---  ]
+yearInput : Html Msg
+yearInput = Form.group [] [
+    Input.number [Input.id "calendar-year", Input.onInput ChangeYear, Input.placeholder "Year"]
+  ]
 
 generateCalendar : Model -> Generator.Calendar.LinearCalendar
 generateCalendar model = generate (CalendarProperties (getOrDefault (model.year) 2018))
